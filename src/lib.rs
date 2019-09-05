973c055f82e75dd8b78912ab9b3f873471d7919c
Hadrien G <knights_of_ni@gmx.com>
2019-09-06 00:17:55 +0200

    Provide a RefUnwindSafe implementation when 'std' feature is enabled

--- src/lib.rs
+++ src/lib.rs
@@ -31,20 +31,28 @@
 //! are often used for lazy global initialization.
 
 #![warn(missing_docs)]
-#![no_std]
+#![cfg_attr(
+    not(feature = "std"), no_std
+)]
 #![cfg_attr(
     feature = "nightly", feature(const_fn, cfg_target_has_atomic, atomic_min_max)
 )]
 
-#[cfg(test)]
+#[cfg(all(test, not(feature = "std")))]
 #[macro_use]
 extern crate std;
+
+#[cfg(feature = "std")]
+use std as core;
 
 // Re-export some useful definitions from libcore
 pub use core::sync::atomic::{fence, Ordering};
 
 use core::cell::UnsafeCell;
 use core::fmt;
+
+#[cfg(feature = "std")]
+use std::panic::RefUnwindSafe;
 
 mod fallback;
 mod ops;
@@ -57,6 +65,16 @@ pub struct Atomic<T: Copy> {
 
 // Atomic<T> is only Sync if T is Send
 unsafe impl<T: Copy + Send> Sync for Atomic<T> {}
+
+// Given that atomicity is guaranteed, Atomic<T> is RefUnwindSafe if T is
+//
+// This is trivially correct for native lock-free atomic types. For those whose
+// atomicity is emulated using a spinlock, it is still correct because the
+// `Atomic` API does not allow doing any panic-inducing operation after writing
+// to the target object.
+//
+#[cfg(feature = "std")]
+impl<T: Copy + RefUnwindSafe> RefUnwindSafe for Atomic<T> {}
 
 impl<T: Copy + Default> Default for Atomic<T> {
     #[inline]
